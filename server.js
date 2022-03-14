0ef26264b828438c2e9488932c8bed6086a86762
Vijay Rao <vjrao.91@gmail.com>
2022-03-13 21:28:05 -0400

    adding Heroku

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
 });
 
 app.get("./notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
 });
 
 app.get("/api/notes", (req, res) => {
@@ -43,11 +43,11 @@ app.delete("api/notes/:id", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
 });
 
 app.get("./notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
 });
 
 app.listen(PORT, function () {
