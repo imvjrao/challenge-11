3f6068677e7a3c70d767034478f0f7fa658b6138
Vijay Rao <vjrao.91@gmail.com>
2022-03-13 21:35:33 -0400

    adding heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const uuid = require("uuid");
 
 
 const app = express ();
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "./Develop/public")));
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -18,20 +18,20 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
 });
 
-app.get("./notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
 });
 
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./db/db.json"))
+    res.sendFile(path.join(__dirname, "./Develop/db/db.json"))
 });
 
 app.post("/api/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const notes = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
     const newNotes = req.body;
     newNotes.id = uuid.v1();
     notes.push(newNotes);
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes));
     res.json(notes);
 });
 
